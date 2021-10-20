d0570694648ea96e61b87493eeb408b182da5188
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-10-20 12:31:08 +0700

    check email exist

--- src/screens/AddOfficerScreen/index.js
+++ src/screens/AddOfficerScreen/index.js
@@ -14,7 +14,12 @@ const AddOfficerScreen = () => {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [ConfirmPassword, setConfirmPassword] = useState("");
+  const [exist, setexist] = useState(Boolean);
   const history = useHistory();
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
 
   const checkFirstName = (firstname) => {
     const data = firstname.target.value;
@@ -66,6 +71,16 @@ const AddOfficerScreen = () => {
     console.log(confirmPassword);
   };
 
+  const isEmailExist = (Email) => {
+    try {
+      axios.get(`${server.ADD_OFFICER}/${Email}`).then((res) => {
+        setexist(res.data);
+      });
+    } catch (error) {
+      return error;
+    }
+  };
+
   const handleSubmit = () => {
     let user = {
       firstname: FirstName,
@@ -92,28 +107,36 @@ const AddOfficerScreen = () => {
       } else if (!regThaiChar.test(LastName)) {
         window.alert("โปรดกรอกนามสกุลเป็นภาษาไทย");
       } else {
-        axios
-          .post(server.ADD_OFFICER, {
-            FirstName: FirstName,
-            LastName: LastName,
-            Phone: Phone,
-            Position: Position,
-            Email: Email,
-            Password: Password,
-          })
-          .then((res) => {
-            const profile = res.data;
-            history.push({
-              pathname: `/confirmaddofficer`,
-              state: {
-                firstname: profile.FirstName,
-                lastname: profile.LastName,
-                phone: profile.Phone,
-                position: profile.Position,
-                email: profile.Email,
-              },
+        isEmailExist(Email);
+        if (exist == false) {
+          window.alert(
+            "ลงทะเบียนผิดพลาด เนื่องจากอีเมล์นี้มีบัญชีผู้ใช้ในระบบแล้ว"
+          );
+          refreshPage();
+        } else {
+          axios
+            .post(server.ADD_OFFICER, {
+              FirstName: FirstName,
+              LastName: LastName,
+              Phone: Phone,
+              Position: Position,
+              Email: Email,
+              Password: Password,
+            })
+            .then((res) => {
+              const profile = res.data;
+              history.push({
+                pathname: `/confirmaddofficer`,
+                state: {
+                  firstname: profile.FirstName,
+                  lastname: profile.LastName,
+                  phone: profile.Phone,
+                  position: profile.Position,
+                  email: profile.Email,
+                },
+              });
             });
-          });
+        }
       }
     } catch (error) {
       return error;
@@ -128,122 +151,122 @@ const AddOfficerScreen = () => {
           <p className="text-xl font-semibold">สำหรับผู้ดูแลระบบ </p>
           <hr className="mt-6 border-b-1 border-blueGray-300" />
           {/* <form className="mt-6"> */}
-            <div className="flex justify-between gap-3">
-              <span className="w-1/2">
-                <label className="block text-xs font-semibold text-gray-600 uppercase">
-                  ชื่อจริง
-                </label>
-                <input
-                  type="text"
-                  name="FirstName"
-                  placeholder="โปรดกรอกชื่อจริง "
-                  onChange={checkFirstName}
-                  className="block b-2 w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-              <span className="w-1/2">
-                <label className="block text-xs font-semibold text-gray-600 uppercase">
-                  นามสกุล
-                </label>
-                <input
-                  type="text"
-                  name="LastName"
-                  placeholder="โปรดกรอกนามสกุล "
-                  onChange={checkLastName}
-                  className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-            </div>
-            <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
-              หมายเลขโทรศัพท์{" "}
-            </label>
-            <input
-              type="tel"
-              name="Phone"
-              placeholder="โปรดกรอกหมายเลขโทรศัพท์"
-              onChange={checkPhone}
-              required
-              className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-            />
-            <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
-              ตำแหน่งงาน
-            </label>
-            <select
-              required={require}
-              id="position"
-              name="Position"
-              className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-              onClick={handlePosition}
+          <div className="flex justify-between gap-3">
+            <span className="w-1/2">
+              <label className="block text-xs font-semibold text-gray-600 uppercase">
+                ชื่อจริง
+              </label>
+              <input
+                type="text"
+                name="FirstName"
+                placeholder="โปรดกรอกชื่อจริง "
+                onChange={checkFirstName}
+                className="block b-2 w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+                required
+              />
+            </span>
+            <span className="w-1/2">
+              <label className="block text-xs font-semibold text-gray-600 uppercase">
+                นามสกุล
+              </label>
+              <input
+                type="text"
+                name="LastName"
+                placeholder="โปรดกรอกนามสกุล "
+                onChange={checkLastName}
+                className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+                required
+              />
+            </span>
+          </div>
+          <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
+            หมายเลขโทรศัพท์{" "}
+          </label>
+          <input
+            type="tel"
+            name="Phone"
+            placeholder="โปรดกรอกหมายเลขโทรศัพท์"
+            onChange={checkPhone}
+            required
+            className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+          />
+          <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
+            ตำแหน่งงาน
+          </label>
+          <select
+            required={require}
+            id="position"
+            name="Position"
+            className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+            onClick={handlePosition}
+          >
+            <option disabled selected value>
+              {" "}
+              โปรดระบุตำแหน่งงาน
+            </option>
+            <option className="option" value="แพทย์">
+              {" "}
+              แพทย์{" "}
+            </option>
+            <option className="option" value="เจ้าหน้าที่">
+              เจ้าหน้าที่
+            </option>
+            <option className="option" value="ผู้ดูแลระบบ">
+              ผู้ดูแลระบบ
+            </option>
+          </select>
+          <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
+            อีเมลล์{" "}
+          </label>
+          <input
+            type="email"
+            name="Email"
+            placeholder="โปรดกรอกอีเมลล์"
+            onChange={checkEmail}
+            required
+            className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+          />
+
+          <div className="flex justify-between gap-3">
+            <span className="w-1/2">
+              <label className="mt-2 block text-xs font-semibold text-gray-600 uppercase">
+                รหัสผ่าน
+              </label>
+              <input
+                type="password"
+                name="Password"
+                placeholder="โปรดกรอกรหัสผ่าน"
+                onChange={handlePassword}
+                className="block b-2 w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+                required
+              />
+            </span>
+            <span className="w-1/2">
+              <label className=" mt-2 block text-xs font-semibold text-gray-600 uppercase">
+                ยืนยันรหัสผ่าน
+              </label>
+              <input
+                type="password"
+                name="ConfirmPassword"
+                placeholder="โปรดยืนยันรหัสผ่านอีกครั้ง"
+                onChange={handleConfirmPassword}
+                className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
+                required
+              />
+            </span>
+          </div>
+
+          <div className="mt-2">
+            {/* <Link to="/confirmaddofficer"> */}
+            <button
+              variant="secondary"
+              className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
+              onClick={handleSubmit}
             >
-              <option disabled selected value>
-                {" "}
-                โปรดระบุตำแหน่งงาน
-              </option>
-              <option className="option" value="แพทย์">
-                {" "}
-                แพทย์{" "}
-              </option>
-              <option className="option" value="เจ้าหน้าที่">
-                เจ้าหน้าที่
-              </option>
-              <option className="option" value="ผู้ดูแลระบบ">
-                ผู้ดูแลระบบ
-              </option>
-            </select>
-            <label className="block mt-2 text-xs font-semibold text-gray-600 uppercase">
-              อีเมลล์{" "}
-            </label>
-            <input
-              type="email"
-              name="Email"
-              placeholder="โปรดกรอกอีเมลล์"
-              onChange={checkEmail}
-              required
-              className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-            />
-
-            <div className="flex justify-between gap-3">
-              <span className="w-1/2">
-                <label className="mt-2 block text-xs font-semibold text-gray-600 uppercase">
-                  รหัสผ่าน
-                </label>
-                <input
-                  type="password"
-                  name="Password"
-                  placeholder="โปรดกรอกรหัสผ่าน"
-                  onChange={handlePassword}
-                  className="block b-2 w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-              <span className="w-1/2">
-                <label className=" mt-2 block text-xs font-semibold text-gray-600 uppercase">
-                  ยืนยันรหัสผ่าน
-                </label>
-                <input
-                  type="password"
-                  name="ConfirmPassword"
-                  placeholder="โปรดยืนยันรหัสผ่านอีกครั้ง"
-                  onChange={handleConfirmPassword}
-                  className="block w-full p-3 mt-2 text-gray-700 bg-white-200 appearance-none focus:outline-none focus:bg-white-300 focus:shadow-inner"
-                  required
-                />
-              </span>
-            </div>
-
-            <div className="mt-2">
-              {/* <Link to="/confirmaddofficer"> */}
-              <button
-                variant="secondary"
-                className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
-                onClick={handleSubmit}
-              >
-                เพิ่มบุคคลากร
-              </button>
-              {/* </Link> */}
-            </div>
+              เพิ่มบุคคลากร
+            </button>
+            {/* </Link> */}
+          </div>
           {/* </form> */}
         </div>
       </div>
