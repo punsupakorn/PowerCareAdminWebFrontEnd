5a7e76b7d6794bd315e136d963aa7f33f119e07c
Daris <daris.pb@mail.kmutt.ac.th>
2021-10-29 21:18:38 +0700

    update med

--- src/screens/MedicineScreen/index.js
+++ src/screens/MedicineScreen/index.js
@@ -11,12 +11,7 @@ import { Modal } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 export default function MedicineScreen() {
-  // const [modalOpen, setModalOpen] = useState(false);
-  const [Name, setName] = useState("");
-  const [Description, setDescription] = useState("");
-  const [Price, setPrice] = useState("");
-  const [Type, setType] = useState("");
-  const [Stock, setStock] = useState("");
+ 
   const [medicine, setMedicine] = useState([]);
 
   const getMedicine = () => {
@@ -28,52 +23,6 @@ export default function MedicineScreen() {
   useEffect(() => {
     getMedicine();
   }, []);
-
-  const handleName = (e) => {
-    const name = e.target.value;
-    setName(name);
-  };
-
-  const handleDescription = (e) => {
-    const description = e.target.value;
-    setDescription(description);
-  };
-
-  const handlePrice = (e) => {
-    const price = e.target.value;
-    setPrice(price);
-  };
-
-  const handleType = (e) => {
-    const type = e.target.value;
-    setType(type);
-  };
-
-  const handleStock = (e) => {
-    const stock = e.target.value;
-    setStock(stock);
-  };
-
-  const handleSubmit = async () => {
-    try {
-      await axios
-        .post(server.MEDICINE, {
-          Name: Name,
-          Description: Description,
-          Price: Price,
-          Type: Type,
-          Stock: Stock,
-        })
-        .then((res) => {
-          console.log(res);
-        });
-    } catch (error) {}
-  };
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const getSkincare = () => {
     axios.get(server.MEDICINE).then((res) => {
@@ -115,30 +64,15 @@ export default function MedicineScreen() {
     });
   };
   // for search
-  const [searchInput, setSearchInput] = useState(null);
   const [searched, setSearched] = useState(false);
-  // data
-  const [data, setData] = useState(mockup);
   // table
-  const [indexTable, setIndexTable] = useState(0);
-  const [numOfRow, setNumOfRow] = useState(10);
-  const numOfTable = Math.ceil(data.length / numOfRow);
-  const numberStartData = indexTable * numOfRow;
-  const dataLength = +numOfRow;
-  const numberEndData =
-    numberStartData + dataLength > data.length
-      ? data.length
-      : numberStartData + dataLength;
-  if (indexTable >= numOfTable) setIndexTable(numOfTable - 1);
-
   // function row data creating
   const RowMedicine = () => {
     // const [disable, setDisable] = useState(amount ? false : true);
     return (
       <div>
         {medicine.map((med) => (
-          <div className="table-grid-medicine content-medicine">
-
+          <div className="table-grid-medicine content-medicine ">
             <p>{med.MedicineName}</p>
             <p className ="text-med" >{med.Price}</p>
             <p>{med.MedicineDescription}</p>
@@ -147,55 +81,14 @@ export default function MedicineScreen() {
           </div>
         ))}
       </div>
-
-      // <div className="menu-row">
-      // <p>{" "}</p>
-      // <p>{Medicine.Type}</p>
-      // {/* <p>{Description}</p>
-      // <p>{name}</p>
-      // <p>{price}</p>
-      // <p>{"111"}</p> */}
-      // </div>
-
-      // ))}
-      // </div>
-      // );
     );
   };
 
-  // data slice with table
-  const rowData = data
-    .slice(numberStartData, numberEndData)
-    .map((item, key) => (
-      <RowMedicine
-        id={item.id}
-        name={item.name}
-        price={item.price}
-        key={key}
-        // index={numberStartData + key}
-        // amount={item.amount}
-        // numberinstock={item.numberinstock}
-      />
-    ));
 
   // refresh page
   function refreshPage() {
     window.location.reload();
   }
-  // function search
-  function search(text) {
-    let nData = [];
-    data.forEach((item, key) => {
-      if (item.name.search(text) != -1) nData.push(item);
-    });
-    setData(nData);
-  }
-
-  // // function submit
-  // const submit = () => {
-  //   const report = data.filter((item) => item.amount != undefined);
-  //   console.log({ report });
-  // };
 
   return (
     <div className="content-body">
@@ -321,19 +214,25 @@ export default function MedicineScreen() {
           {/* end header */}
 
           <div className="body-table-medicine">
-            {/* body table */}
-            {rowData}
+
+              {/* {medicine} */}
+              {medicine.map((med) => (
+                <div className="table-grid-medicine content-medicine">
+                  {/* <p> </p> */}
+                  <p>{med.MedicineName}</p>
+                  <p className ="text-med" > {med.Price}</p>
+                  <p>{med.MedicineDescription}</p>
+                  <p className ="text-med" >{med.Type}</p>
+
+                </div>
+              ))}
+              {/* end body table */}
+
+
             {/* end body table */}
           </div>
         </div>
 
-        {/* <TableController
-          indexTable={indexTable}
-          setIndexTable={setIndexTable}
-          numOfTable={numOfTable}
-          setNumOfRow={setNumOfRow}
-          numOfRow={numOfRow}
-        /> */}
         <div className="px-2 mt-3 ">
           <Link to="/">
             <Button
@@ -345,143 +244,7 @@ export default function MedicineScreen() {
           </Link>{" "}
         </div>
       </div>
-      {/* {modalOpen && <AddMedicineScreen setOpenModal={setModalOpen} />} */}
     </div>
   );
 }
 
-//////////////////////// Mockup Data //////
-
-const mockup = [
-  {
-    id: "C001",
-    name: "ยา A",
-    price: 100,
-    numberinstock: "2",
-  },
-  // {
-  //   id: "C002",
-  //   name: "ยา B",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C003",
-  //   name: "ยา C",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C004",
-  //   name: "ยา D",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C005",
-  //   name: "ยา E",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C006",
-  //   name: "ยา F",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C007",
-  //   name: "ยา G",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C008",
-  //   name: "ยา H",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C009",
-  //   name: "ยา I",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C010",
-  //   name: "ยา J",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C011",
-  //   name: "ยา K",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C012",
-  //   name: "ยา L",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C013",
-  //   name: "ยา M",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C014",
-  //   name: "ยา N",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C015",
-  //   name: "ยา O",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C016",
-  //   name: "ยา P",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C017",
-  //   name: "ยา Q",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C018",
-  //   name: "ยา R",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C019",
-  //   name: "ยา S",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C020",
-  //   name: "ยา T",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C021",
-  //   name: "ยา U",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C022",
-  //   name: "ยา V",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C023",
-  //   name: "ยา W",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C024",
-  //   name: "ยา X",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C025",
-  //   name: "ยา Y",
-  //   price: 100,
-  // },
-  // {
-  //   id: "C026",
-  //   name: "ยา Z",
-  //   price: 100,
-  // },
-];
